4187792ebe9d0b6407449376b8c01cf466e97c8e
KAPIL <kapily2004@yahoo.com>
2023-08-25 23:52:58 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def input_page():
+    if request.method == 'POST':
+        # Retrieve form data
+        data = request.json
+        sites = int(data['sites'])
+        processes = [int(p) for p in data['processes']]
+        s1 = int(data['s1'])
+        p1 = int(data['p1'])
+        sp1 = int(data['sp1'])
+        sp2 = int(data['sp2'])
+
+        # Perform calculations
+        deadlock_detected = False
+        probe_msg = f"\nProbe message is ({p1}, {sp1}, {sp2})"
+        if p1 == sp2:
+            deadlock_detected = True
+
+        # Return the result as JSON
+        return jsonify(deadlock_detected=deadlock_detected,probe_msg=probe_msg)
+
+    # Render the input page template
+    return render_template('input.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
